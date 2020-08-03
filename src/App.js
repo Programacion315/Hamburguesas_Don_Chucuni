99ee744591ed823c5aa760b2cefa98dc0862f025
juanmanuelcrego <juanmanuel.crego@gmail.com>
2020-08-03 11:20:24 -0300

    styles

--- src/App.js
+++ src/App.js
@@ -1,74 +1,75 @@
 import React, { Fragment, useState } from "react";
-
-// Importo el componente Burger
 import Burger from "./components/Burger";
-
-// Importo el componente Cart para el carrito 
 import Cart from "./components/Cart";
+import "./styles/styles.css";
+
+// IMPORTO LAS IMAGENES
+import Salteña from "./images/Salteña.png";
+import Porteña from "./images/Porteña.png";
+import Chicken from "./images/Chicken.png";
+import Criolla from "./images/Criolla.png";
+import Patagonia from "./images/patagonia.png";
 
 function App() {
-
-  // Hacemos dos states, uno para el listado de burgers y otro
-  // para el cart de compras 
-  // El state siempre tiene dos parámetros, uno es
-  // el estado puro y el otro es la función que cambiará
-  // ese estado, el estado no puede cambiarse directamente
-
-  // Listado de burgers en el State
   const [burgers, setBurgers] = useState([
-    { id: 1, nombre: "Hamburguesa completa", precio: 250 },
-    { id: 2, nombre: "Hamburguesa con cheddar", precio: 180 },
-    { id: 3, nombre: "Hamburguesa con jamón y queso", precio: 150 },
-    { id: 4, nombre: "Hamburguesa con bacon", precio: 200 },
-    { id: 5, nombre: "Hamburguesa de pollo completa", precio: 240 },
+    { id: 5, image: Salteña, nombre: "SALTEÑA", precio: 250 },
+    { id: 2, image: Porteña, nombre: "PORTEÑA", precio: 180 },
+    { id: 3, image: Chicken, nombre: "CHICKEN", precio: 150 },
+    { id: 4, image: Criolla, nombre: "CRIOLLA", precio: 200 },
+    { id: 1, image: Patagonia, nombre: "PATAGONIA", precio: 240 },
   ]);
 
-  // Cart de compras en el state, dejo vacío el useState ya que en 
-  // dicho array se irán agregando burgers, por ende debe estar vacío en 
-  // un principio 
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
 
   return (
     <Fragment>
-      <h1>Nuestras burgers</h1>
-      {/* Utilizo el map para recorrer y retornar los valores
-      del objeto burgers, la forma de hacerlo es simple 
-      pongo burgers.map y voy a acceder a cada burger 
-      especificando el nombre, en este caso burger */}
-      {burgers.map((burger) => (
-        // Creo otro componente, llamado Burger, para 
-        // imprimir ahí el listado de las burgers y no 
-        // tener tanto código en este componente App
-        //------------------------------------------
-        //Le especifico la prop burger haciendo referencia
-        //a cada burger, las cuales están
-        //en el objeto y lo más importante, le paso como prop
-        //el id en key, para que cada burger sea única
-        <Burger 
-        key={burger.id}
-        burger={burger} 
-        // Agrego la prop del cart, para que se puedan agregar las 
-        // burgers al carrito
-        cart={cart}
-        setCart={setCart}
-        // Agrego la prop de burgers directamente, del estado burgers esto 
-        // es para poder obtener todos los datos del objeto cuando hacemos 
-        // la "compra" de la burger 
-        burgers={burgers}
-        />
-      ))}
+      {/* SECTION CONTENEDOR GLOBAL */}
+      <section className="container">
+        {/* SECTION DE LA CABECERA */}
+        <section className="container__header">
+          {/* BOTON DE LA CABECERA */}
+          <div className="container__header__menu">
+            <button className="container__header__menu--button"></button>
+          </div>
 
-        {/* Incorporo el componente Cart para el carrito */}
-        <Cart 
-        //  Le paso las props del cart state al componente Cart para que 
-        //  pueda imprimir los objetos del state cart o carrito
-        cart={cart}
-        // Para que no tire error de undefined cuando hagamos uso del botón delete
-        // necesitamos que el componente cart extraiga la función prop del setCart
-        setCart={setCart}
-        />
+          {/* TITULO DE LA CABECERA */}
+          <div className="container__header__title">
+            <h3 className="container__header__title--text">HAMBURGER</h3>
+          </div>
 
+          {/* CART DE LA CABECERA */}
+          <div className="container__header__cartDiv">
+            <Cart cart={cart} setCart={setCart} />
+          </div>
+        </section>
 
+        {/* SECTION CONTENEDOR PRINCIPAL */}
+        <section className="container__content">
+
+          <div className="container__content__title">
+            <h1>Aprovecha solo por tiempo limitado combos 50% off!</h1>
+          </div>
+
+          <div className="container__content__listBurgers">
+            {burgers.map((burger) => (
+              <Burger
+                key={burger.id}
+                burger={burger}
+                cart={cart}
+                setCart={setCart}
+                burgers={burgers}
+              />
+            ))}
+          </div>
+
+        </section>
+
+        {/* SECTION FOOTER */}
+        <section className="container__footer">
+          <p>HAMBURGER 2020 - Todos los derechos reservados</p>
+        </section>
+        
+      </section>
     </Fragment>
   );
 }
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,28 +1,59 @@
-import React from "react";
-
-// Importo el componente de Burger para imprimirlo en el cart una vez que
-// se itera con map sobre el array del carrito
+import React, { Fragment } from "react";
 import Burger from "./Burger";
+import "../styles/styles.css";
 
-// Extraigo la prop del componente padre App, y traigo el state del cart, además 
-// también extraigo la función setCart que modifica el estado del cart, esto 
-// lo hago para que al utilizar el botón delete no marque error de undefined, ya que 
-// los componentes hijos deben pasarse también las props heredadas del padre para no 
-// generar algunos errores que podríamos solucionar con por ejemplo Redux, pero no lo 
-// vamos a utilizar en este caso 
-const Cart = ({ cart, setCart }) => (
-  <div>
-    <h2>Cart</h2>
+const Cart = ({ cart, setCart }) => {
+  const viewCart = () => {
+    {
+      document.querySelector(
+        "div.container__header__cartDiv--list"
+      ).style.display = "none"
+        ? (document.querySelector(
+            "div.container__header__cartDiv--list"
+          ).style.display = "flex")
+        : (document.querySelector(
+            "div.container__header__cartDiv--list"
+          ).style.display = "none");
+    }
+  };
 
-    {/* Utilizo un ternario para la visualización de las
-        burgers en el componente del carrito  */}
+  const closeDiv = () => {
+    document.querySelector(
+      "div.container__header__cartDiv--list"
+    ).style.display = "none";
+  };
 
-    {cart.length === 0 ? (
-      <p>¿Todavía no elegiste?</p>
-    ) : (
-      cart.map((burger) => <Burger key={burger.id} burger={burger} cart={cart} setCart={setCart} />)
-    )}
-  </div>
-);
-
+  return (
+    <Fragment>
+      <div className="container__header__cartDiv">
+        <p className="container__header__cartDiv--count">{cart.length}</p>
+        <button
+          className="container__header__cartDiv--btnCart"
+          onClick={() => viewCart()}
+        ></button>
+      </div>
+      <div className="container__header__cartDiv--list">
+        <button
+          className="container__content__burgers--btnClose"
+          type="button"
+          onClick={() => closeDiv()}
+        >
+          X
+        </button>
+        {cart.length === 0 ? (
+          <p>No hay nada por aquí...</p>
+        ) : (
+          cart.map((burger) => (
+            <Burger
+              key={burger.id}
+              burger={burger}
+              cart={cart}
+              setCart={setCart}
+            />
+          ))
+        )}
+      </div>
+    </Fragment>
+  );
+};
 export default Cart;
